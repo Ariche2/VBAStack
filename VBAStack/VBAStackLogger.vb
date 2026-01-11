f81e16526760eccf2718adc3a579cee67cff0f55
l.burbridge <leo@redrosesoftware.co.uk>
2026-01-11 14:05:56 +0000

    testing different method of including dbghelp/symsrv

---
+++ VBAStack/VBAStackLogger.vb
@@ -0,0 +1,72 @@
+''' <summary>
+''' Centralized logging module for VBAStack project.
+''' </summary>
+Friend Module VBAStackLogger
+
+    ''' <summary>
+    ''' Log severity levels.
+    ''' </summary>
+    Public Enum LogLevel
+        Debug
+        Info
+        Warning
+        [Error]
+    End Enum
+
+    ''' <summary>
+    ''' When True, only Error level messages are logged. When False, all messages are logged.
+    ''' </summary>
+    Public Property ErrorsOnly As Boolean = False
+
+    ''' <summary>
+    ''' Logs a debug message.
+    ''' </summary>
+    Public Sub LogDebug(message As String)
+        Log(LogLevel.Debug, message)
+    End Sub
+
+    ''' <summary>
+    ''' Logs an informational message.
+    ''' </summary>
+    Public Sub LogInfo(message As String)
+        Log(LogLevel.Info, message)
+    End Sub
+
+    ''' <summary>
+    ''' Logs a warning message.
+    ''' </summary>
+    Public Sub LogWarning(message As String)
+        Log(LogLevel.Warning, message)
+    End Sub
+
+    ''' <summary>
+    ''' Logs an error message.
+    ''' </summary>
+    Public Sub LogError(message As String)
+        Log(LogLevel.Error, message)
+    End Sub
+
+    ''' <summary>
+    ''' Core logging method that respects the ErrorsOnly setting.
+    ''' </summary>
+    Private Sub Log(level As LogLevel, message As String)
+        If ErrorsOnly AndAlso level <> LogLevel.Error Then
+            Return
+        End If
+
+        Dim prefix As String = String.Empty
+        Select Case level
+            Case LogLevel.Debug
+                prefix = "[DEBUG]"
+            Case LogLevel.Info
+                prefix = "[INFO]"
+            Case LogLevel.Warning
+                prefix = "[WARNING]"
+            Case LogLevel.Error
+                prefix = "[ERROR]"
+        End Select
+
+        Debug.WriteLine($"{prefix} {message}")
+    End Sub
+
+End Module
--- VBAStack/VBECallstackProvider.vb
+++ VBAStack/VBECallstackProvider.vb
@@ -15,8 +15,8 @@ Public Class VBECallstackProvider
         Try
             s_IsVerified = VBESymbolResolver.VerifyAllPointers()
         Catch ex As Exception
-            Debug.WriteLine($"[VBECallstackProvider] Error verifying pointers: {ex.Message}")
-            Debug.WriteLine(ex.StackTrace)
+            VBAStackLogger.LogError($"[VBECallstackProvider] Error verifying pointers: {ex.Message}")
+            VBAStackLogger.LogError(ex.StackTrace)
             s_IsVerified = False
         End Try
 
@@ -28,9 +28,10 @@ Public Class VBECallstackProvider
     ''' </summary>
     ''' <param name="vbe">A VBE object from an Office application.</param>
     ''' <returns>A formatted string containing the callstack, or an error message.</returns>
-    Public Shared Function GetCallstack(vbe As Object) As String
+    Public Shared Function GetCallstack(vbe As Object, Optional ExcludeNonBasicCodeFrames As Boolean = False) As String
         Dim originalVisibility As Boolean
 
+        ' Validate VBE version and check if it's currently visible
         Try
             If CStr(vbe.Version).FirstOrDefault() <> "7"c Then
                 Return "VBE version is less than 7.0 - VBAStack only works with VBE7+"
@@ -41,6 +42,7 @@ Public Class VBECallstackProvider
             Throw New Exception("Could not access VBE object. See inner exception.", ex)
         End Try
 
+        ' Get VBE window handle
         Dim vbeHwnd As IntPtr
         Try
             vbeHwnd = New IntPtr(CLng(vbe.MainWindow.HWnd))
@@ -48,6 +50,7 @@ Public Class VBECallstackProvider
             Throw New Exception("Could not get VBE window handle. See inner exception.", ex)
         End Try
 
+        ' Hook VBE window to control visibility
         Dim hook As VBEWindowHook
         Try
             hook = New VBEWindowHook(vbeHwnd) With {
@@ -57,23 +60,30 @@ Public Class VBECallstackProvider
             Throw New Exception("Could not hook VBE window. See inner exception.", ex)
         End Try
 
+
+        ' Go get function pointers, do our best to verify them so we don't hard crash
         If Not VerifyPointers() Then
             Throw New Exception("Could not get pointers to necessary VBE7 functions")
         End If
 
+        ' Save the original mode to restore later
         Dim originalMode As EbMode = VBENativeWrapper.GetMode()
 
         If originalMode <> EbMode.Break Then
+            ' Switch to Break mode to read the callstack
             VBENativeWrapper.SetMode(EbMode.Break)
+
+            ' Verify we successfully switched modes
+            If VBENativeWrapper.GetMode() <> EbMode.Break Then
+                Throw New Exception("Could not set EbMode to 'Break'")
+            End If
         End If
 
-        If VBENativeWrapper.GetMode() <> EbMode.Break Then
-            Throw New Exception("Could not set EbMode to 'Break'")
-        End If
-
+        ' Get number of frames on the stack
         Dim callStackCount As Integer = VBENativeWrapper.GetCallstackCount()
         Dim result As New Text.StringBuilder()
 
+        ' Read each stack frame, from most recent to oldest, and add to result
         If callStackCount > 0 Then
             For i As Integer = callStackCount - 1 To 0 Step -1
                 Try
@@ -85,26 +95,32 @@ Public Class VBECallstackProvider
                         Dim functionName As String = parts(2)
                         result.AppendLine($"{moduleName}::{functionName}")
                     Else
+                        ' Not sure why but on 64bit Office, there's one of these between each normal stack frame? Have an option to ignore them.
                         If callStackStr = "[<Non-Basic Code>]" Then
-                            result.AppendLine(callStackStr)
+                            If Not ExcludeNonBasicCodeFrames Then
+                                result.AppendLine(callStackStr)
+                            End If
                         Else
                             result.AppendLine($"Unparseable stack entry: {callStackStr}")
                         End If
                     End If
                 Catch ex As Exception
-                    result.AppendLine($"Could not read stack at index {i} - exception: {ex.Message}")
+                    result.AppendLine($"Exception when reading stackframe {i}: {ex.Message}")
                 End Try
             Next
         End If
 
+        ' Restore original VBE mode
         VBENativeWrapper.SetMode(originalMode)
 
+        ' Restore original VBE window visibility
         If originalVisibility = True Then
             hook.EnsureShown()
         Else
             hook.EnsureHidden()
         End If
 
+        ' Release the hook on the VBE window
         hook.ReleaseHandle()
 
         Return result.ToString()
